91bc1202f0d708321f6176bd03bdeaa9e7fc26dd
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-17 12:54:33 +0100

    ci & release workflows

--- src/adapter/interface.ts
+++ src/adapter/interface.ts
@@ -1,9 +1,38 @@
+/**
+ * Examples of how referencing properties could be named
+ * {
+ *   portfolioId: '123',
+ * },
+ * {
+ *   accountIds: ['123', '456'],
+ * },
+ * {
+ *   account: {
+ *     accountId: '123',
+ *   },
+ * }
+ * {
+ *   accounts: [
+ *     {
+ *       accountId: '123',
+ *     },
+ *     {
+ *       accountId: '456',
+ *     }
+ *   ]
+ * },
+ */
+
 export type ReferencingPropertyName = `${string}Id`;
+export type ReferencingPropertyArrayName = `${string}Ids`;
 export type ReferencingDocumentChunk = {
-  type?: string;
-  chunkType: 'object' | 'array';
+  // Literal properties - always constants, that identify the type of the document
+  // e.g. type: 'portfolio' or type: 'account', countryCode: 'DE', accountType: 'statePension'
+  [literal: string]: string;
 } & {
-  [key: ReferencingPropertyName]: undefined | ReferencingDocumentChunk;
+  // This could be the actual referencing property or a property holding child object/array or array of referencing properties
+  // TODO: How do we handle this?
+  [referencingProperty: ReferencingPropertyName]: undefined | ReferencingDocumentChunk;
 };
 export interface DocumentSchemaAdapter {
   findReferencingChunks(): ReferencingDocumentChunk;
