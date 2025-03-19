6b49dbd60547a3e04a8a0d3cbe1768e7ed478d11
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-19 15:35:22 +0100

    build

--- src/adapter/zod.ts
+++ src/adapter/zod.ts
@@ -1,18 +1,18 @@
-import { type ZodSchema } from 'zod';
-import { DocumentSchemaAdapter, ReferencingDocumentChunk } from './interface';
+// import { type ZodSchema } from 'zod';
+// import { DocumentSchemaAdapter, ReferencingDocumentChunk } from './interface';
 
-export class ZodDocumentSchemaAdapter implements DocumentSchemaAdapter {
-  #schema: ZodSchema;
-  constructor(schema: ZodSchema) {
-    this.#schema = schema;
-  }
-  findReferencingChunks(): ReferencingDocumentChunk {
-    this.#schema.parse({});
-    return {
-      chunkType: 'object'
-    };
-  }
-  findReferencingFields(): string[] {
-    return [];
-  }
-}
+// export class ZodDocumentSchemaAdapter implements DocumentSchemaAdapter {
+//   #schema: ZodSchema;
+//   constructor(schema: ZodSchema) {
+//     this.#schema = schema;
+//   }
+//   findReferencingChunks(): ReferencingDocumentChunk {
+//     this.#schema.parse({});
+//     return {
+//       chunkType: 'object'
+//     };
+//   }
+//   findReferencingFields(): string[] {
+//     return [];
+//   }
+// }
