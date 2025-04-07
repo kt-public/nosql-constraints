73a1c77ac23b1af696e8cf616f2300d06f9d9813
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-07 13:17:29 +0200

    add and test constraints

--- tests/unit/constraints/factory.spec.ts
+++ tests/unit/constraints/factory.spec.ts
@@ -26,14 +26,20 @@ const container2DocSchema1 = z.object({
   type: z.literal('C2A'),
   title: z.string(),
   description: z.string(),
-  buddyId: z.string()
+  buddyId: z.string(),
+  somePartitionKey: z.object({
+    someBuddyId: z.string()
+  })
 });
 const container2DocSchema2 = z.object({
   id: z.string(),
   type: z.literal('C2B'),
   title: z.string(),
   description: z.string(),
-  buddyIds: z.array(z.string())
+  buddyIds: z.array(z.string()),
+  somePartitionKey: z.object({
+    someBuddyId: z.string()
+  })
 });
 const container2DocSchema3 = z.object({
   id: z.string(),
@@ -45,7 +51,11 @@ const container2DocSchema3 = z.object({
       parentId: z.string(),
       comment: z.string()
     })
-  )
+  ),
+  somePartitionKey: z.object({
+    someBuddyId: z.string()
+  }),
+  compoundId: z.string()
 });
 const container2DocSchema = z.discriminatedUnion('type', [
   container2DocSchema1,
@@ -70,6 +80,34 @@ describe('Constraint factory', () => {
         { containerId: 'container1', refDocType: { type: 'C1A' } }
       );
     });
+    it('should fail to add: container2/doc1.buddyId -> container1/doc1.id', ({ expect }) => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      expect(() => {
+        factory.addDocument2DocumentConstraint(
+          { containerId: 'container2', refDocType: { type: 'C2A' } },
+          { refProperties: { buddyIdx: 'id' } },
+          { containerId: 'container1', refDocType: { type: 'C1A' } }
+        );
+      }).toThrowError(
+        'Failed to validate referencing constraint container2/{"type":"C2A"}/buddyIdx: property not found'
+      );
+    });
+    it('should fail to add: container2/doc1.buddyId -> container1/doc1.id', ({ expect }) => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      expect(() => {
+        factory.addDocument2DocumentConstraint(
+          { containerId: 'container2', refDocType: { type: 'C2A' } },
+          { refProperties: { buddyId: 'idx' } },
+          { containerId: 'container1', refDocType: { type: 'C1A' } }
+        );
+      }).toThrowError(
+        'Failed to validate referenced constraint container1/{"type":"C1A"}/idx: property not found'
+      );
+    });
     it('should be able to add: container2/doc2.buddyIds -> container1/doc1.id', () => {
       const factory = new ConstraintFactory();
       factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
@@ -78,6 +116,58 @@ describe('Constraint factory', () => {
         { containerId: 'container2', refDocType: { type: 'C2B' } },
         { refProperties: { 'buddyIds.[]': 'id' } },
         { containerId: 'container1', refDocType: { type: 'C1A' } }
+      );
+    });
+  });
+  describe('Partition 2 Document constraint', () => {
+    it('should be able to add: container2/["somePartitionKey.someBuddyId"] -> container1/doc1.id', () => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      factory.addPartition2DocumentConstraint(
+        { containerId: 'container2', partitionKeyProperties: ['somePartitionKey.someBuddyId'] },
+        { refProperties: { 'somePartitionKey.someBuddyId': 'id' } },
+        { containerId: 'container1', refDocType: { type: 'C1A' } }
+      );
+    });
+    it('should fail to add: container2/["somePartitionKey.someBuddyId"] -> container1/doc1.id', ({
+      expect
+    }) => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      expect(() => {
+        factory.addPartition2DocumentConstraint(
+          { containerId: 'container2', partitionKeyProperties: ['somePartitionKey.someBuddyId'] },
+          { refProperties: { somePartitionKey: 'id' } },
+          { containerId: 'container1', refDocType: { type: 'C1A' } }
+        );
+      }).toThrowError(
+        'Failed to validate referencing constraint container2/["somePartitionKey.someBuddyId"]: all of the partition keys must be present in the keys of the constraint.refProperties'
+      );
+    });
+  });
+  describe('Document compound constraint', () => {
+    it('should be able to add compound: container2/doc3.compoundId -> container2/doc3.compoundId', () => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      factory.addDocumentCompoundConstraint(
+        { containerId: 'container2', refDocType: { type: 'C2C' } },
+        { compoundProperties: ['compoundId'] }
+      );
+    });
+    it('should fail to add compound: container2/doc3.compoundId -> container2/doc3.compoundId', ({ expect }) => {
+      const factory = new ConstraintFactory();
+      factory.addDocumentSchema('container1', zod(testCaseSchemas.container1));
+      factory.addDocumentSchema('container2', zod(testCaseSchemas.container2));
+      expect(() => {
+        factory.addDocumentCompoundConstraint(
+          { containerId: 'container2', refDocType: { type: 'C2C' } },
+          { compoundProperties: ['compoundId', 'compoundIdx'] }
+        );
+      }).toThrowError(
+        'Failed to validate compound constraint container2/{"type":"C2C"}/compoundId,compoundIdx: compound properties must be present in each of the referenced document schema chunks'
       );
     });
   });
