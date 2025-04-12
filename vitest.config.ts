5d4f6ea831bcad0845e2beef2306914983e170b3
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-12 16:26:12 +0200

    sonar coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'istanbul' // or 'v8'
+      provider: 'istanbul', // or 'v8'
       // provider: 'v8'
+      reporter: ['text', 'html', 'clover', 'json', 'lcov']
     }
   }
 });
